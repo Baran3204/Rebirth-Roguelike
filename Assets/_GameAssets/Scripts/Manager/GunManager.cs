9c93e04c16418eb59cc63321b214ac69cc4adc96
Baran3204 <brngumus1345@gmail.com>
2025-03-14 20:33:32 +0300

    Bullet Added to Character

---
+++ Assets/_GameAssets/Scripts/Manager/GunManager.cs
@@ -0,0 +1,48 @@
+using System;
+using TMPro;
+using UnityEditor;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class GunManager : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] private Transform _gunTransform;
+    [SerializeField] private GameObject _bullet;
+
+    [Header("Settings")]
+
+    [SerializeField] private float _bulletSpeed;
+
+    private float _Direction;
+
+
+    private void Update() 
+    {
+        SetInputs();
+        CreateBullet();   
+    }
+
+    private void CreateBullet()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            GameObject bullet = Instantiate(_bullet, _gunTransform.position, Quaternion.identity);
+
+            Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+
+            rb.linearVelocity = _gunTransform.transform.right * _bulletSpeed;
+
+            Destroy(bullet, 5f);
+        }
+    }
+    private void SetInputs()
+    {
+        var mouseDir = Input.mousePosition - Camera.main.WorldToScreenPoint(_gunTransform.position);
+         
+        _Direction = Mathf.Atan2(mouseDir.y, mouseDir.x) * Mathf.Rad2Deg;
+
+        _gunTransform.rotation = Quaternion.AngleAxis(_Direction, Vector3.forward);
+    }
+}
